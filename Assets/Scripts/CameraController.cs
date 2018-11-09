4f3724c086eebe66364f4583a83f28d66bc56534
Diego Doná <diedona@gmail.com>
2018-11-09 00:27:15 -0200

    start

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float _speed;
+    public Transform[] _views;
+
+    private Transform currentView;
+    private int currentViewNum;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.startView();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            this.nextView();
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        this.moveCamera();
+    }
+
+    /// //////////////////////////
+
+    private void nextView()
+    {
+        this.currentViewNum++;
+        if(this.currentViewNum >= _views.Count())
+        {
+            this.currentViewNum = 0;
+        }
+
+        this.goToView();
+    }
+
+    private void startView()
+    {
+        this.currentViewNum = 0;
+        this.goToView();
+    }
+
+    private void goToView()
+    {
+        this.currentView = _views[this.currentViewNum];
+    }
+
+    private void moveCamera()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, this.currentView.position, Time.deltaTime * _speed);
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, this.currentView.rotation, Time.deltaTime * _speed);
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    public Transform objectToRotate;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.objectToRotate = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.objectToRotate.transform.Rotate(new Vector3(10, 3, 15) * Time.deltaTime);
+    }
+}
